d8400c3ec85b2d6c5f7a62bb171c94187ce593e1
s3no <47964733+s3no@users.noreply.github.com>
2020-07-15 23:41:28 +0800

    Update 3dScanner.py

--- 3dScanner.py
+++ 3dScanner.py
@@ -6,7 +6,27 @@ imgCount = 1
 camera = PiCamera()
 bedControlPins = [7,11,13,15]
 cameraControlPins = [12,16,18,22]
-smallTurn = 
+leftTurnSequence = [
+        [1,0,0,0],
+        [1,1,0,0],
+        [0,1,0,0],
+        [0,1,1,0],
+        [0,0,1,0],
+        [0,0,1,1],
+        [0,0,0,1],
+        [1,0,0,1]
+    ]
+
+rightTurnSequence = [
+        [1,0,0,1],
+        [0,0,0,1],
+        [0,0,1,1],
+        [0,0,1,0],
+        [0,1,1,0],
+        [0,1,0,0],
+        [1,1,0,0],
+        [1,0,0,0]
+    ]
 
 def takePhoto():
     global imgCount
@@ -17,22 +37,13 @@ def takePhoto():
     camera.stop_preview()
     imgCount += 1
 
-def spinMotor(motorSelect):
+def spinMotor(motorSelect,direction):
     GPIO.setmode(GPIO.BOARD)
     control_pins = motorSelect
     for pin in control_pins:
         GPIO.setup(pin, GPIO.OUT)
         GPIO.output(pin, 0)
-    halfstep_seq = [
-        [1,0,0,0],
-        [1,1,0,0],
-        [0,1,0,0],
-        [0,1,1,0],
-        [0,0,1,0],
-        [0,0,1,1],
-        [0,0,0,1],
-        [1,0,0,1]
-    ]
+    halfstep_seq = direction
     for i in range(64):
         for halfstep in range(8):
             for pin in range(4):
@@ -40,12 +51,14 @@ def spinMotor(motorSelect):
             time.sleep(0.001)
     GPIO.cleanup()
 
+
 #only does 8 photos at one angle. Need to build arm to get more photos at other angles
 for x in range(9):
-    spinMotor(bedControlPins)
-    takePhoto()
-    
+    spinMotor(bedControlPins,leftTurnSequence)
+    takePhoto()   
 #Add change camera angle then re-run above loop
-for x in range(30):    
-    spinMotor(cameraControlPins)
-
+for x in range(20):    
+    spinMotor(cameraControlPins,leftTurnSequence)
+#now Go Back
+for x in range(20):    
+    spinMotor(cameraControlPins,rightTurnSequence)
